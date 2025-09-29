636fa2ba5f5959aeadf2b18a107567ae900737c1
Diogny <diognyab@hotmail.com>
2025-09-29 19:26:19 -0400

    index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,23 @@
+import { ISubnetAddress, SubnetAddress } from "./subnet-address";
+import { ISubnetMask, SubnetMask } from "./subnet-mask";
+import { INetworkRange, NetworkRange } from "./network-range";
+import { INetworkClass, NetworkClass } from "./network-class";
+import { IDHCP, DHCP } from "./dhcp";
+import { IIP, IP } from "./ip";
+import { IMAC, MAC } from "./mac";
+import { ICIDR, CIDR } from "./cidr";
+import { ISubnet, Subnet } from "./subnet";
+
+export {
+	ISubnetAddress, SubnetAddress,
+	ISubnetMask, SubnetMask,
+	INetworkRange, NetworkRange,
+	INetworkClass, NetworkClass,
+	IDHCP, DHCP,
+	IIP, IP,
+	IMAC, MAC,
+	ICIDR, CIDR,
+	ISubnet, Subnet
+}
 export { binary, chunkString, chunkRightString, integer, pad, padRight, removeTrailingZeros } from "./lib";
 export { NetworkClassAny, NetworkClassA, NetworkClassB, NetworkClassC, NetworkClassD, NetworkClassE } from "./common";
-export { ISubnetAddress, SubnetAddress } from "./subnet-address";
-export { ISubnetMask, SubnetMask } from "./subnet-mask";
-export { INetworkRange, NetworkRange } from "./network-range";
-export { INetworkClass, NetworkClass } from "./network-class";
-export { IDHCP, DHCP } from "./dhcp";
-export { IIP, IP } from "./ip";
-export { IMAC, MAC } from "./mac";
-export { ICIDR, CIDR } from "./cidr";
-export { ISubnet, Subnet } from "./subnet";
